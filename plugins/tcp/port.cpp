a8af46bdcb4b1ce6224f3a586fdd3aef147c2c19
vfrolov <vfrolov>
2008-08-28 10:31:25 +0000

    Removed linking with ../../utils.h and ../../utils.cpp

--- plugins/tcp/port.cpp
+++ plugins/tcp/port.cpp
@@ -19,6 +19,9 @@
  *
  *
  * $Log$
+ * Revision 1.5  2008/08/28 10:31:25  vfrolov
+ * Removed linking with ../../utils.h and ../../utils.cpp
+ *
  * Revision 1.4  2008/08/15 12:44:59  vfrolov
  * Added fake read filter method to ports
  *
@@ -37,8 +40,17 @@
 #include "comparams.h"
 #include "comport.h"
 #include "import.h"
-#include "../../utils.h"
 
+///////////////////////////////////////////////////////////////
+static const char *GetParam(const char *pArg, const char *pPattern)
+{
+  size_t lenPattern = strlen(pPattern);
+
+  if (_strnicmp(pArg, pPattern, lenPattern) != 0)
+    return NULL;
+
+  return pArg + lenPattern;
+}
 ///////////////////////////////////////////////////////////////
 static PLUGIN_TYPE CALLBACK GetPluginType()
 {
